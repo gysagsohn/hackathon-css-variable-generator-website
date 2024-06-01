8aa5a1fce1470604d936eeb71484c23d6532586c
gysagsohn <gysagsohn@hotmail.com>
2024-06-01 10:52:10 +1000

    feat: routing with template and home page
    End of day 1 class

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Template from "./pages/_TemplatePage";
+import HomePage from "./pages/HomePage";
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Template />}>
+        <Route index element={<HomePage />} />
+        {/* <Route path="generator" element={<GeneratorPage />} />
+        <Route path="generator/saved" element={<SavedThemePage />} />  */}
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+export default function Footer(){
+
+	return (
+		<footer>
+			<h6>Copyright 2024 ColourAcademy based on class work/ Gy Sohn</h6>
+			<a href="mailto:test@email.com">Email us!</a>
+		</footer>
+	)
+}
