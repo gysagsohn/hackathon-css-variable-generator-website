85fdc931674f6a3b0ccdf80a854a12a0926bc104
gysagsohn <gysagsohn@hotmail.com>
2024-06-01 13:15:02 +1000

    end of second class - generate theme

--- src/pages/GeneratorPage.jsx
+++ src/pages/GeneratorPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useBaseColourGlobalData, useBaseColourGlobalDispatch } from "../contexts/baseColourContext";
+import { Sketch } from "@uiw/react-color";
 
 export default function GeneratorPage() {
     // Base Colour from form
@@ -14,6 +15,10 @@ export default function GeneratorPage() {
         setFormBaseColour(baseColourGlobal);
     }, [baseColourGlobal]);
 
+    useEffect(() => {
+        setBaseColourGlobal(formBaseColour);
+    }, [formBaseColour]);
+
     // Function to handle form input change
     const handleInputChange = (e) => {
         const newColour = e.target.value;
@@ -25,11 +30,12 @@ export default function GeneratorPage() {
         <div>
             {/* Base colour input form */}
             <h1>Base Colour: {formBaseColour}</h1>
-            <input 
+            {/* <input 
                 type="color" 
                 value={formBaseColour} 
                 onChange={handleInputChange} 
-            />
+            /> */}
+			<Sketch color={formBaseColour} onChange={(colour) => setFormBaseColour(colour.hex)} />
             {/* CSS theme display component */}
         </div>
     );
