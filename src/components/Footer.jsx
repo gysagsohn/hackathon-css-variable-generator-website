9dec4587d7b4dfd500618789bd3069ec7936c8a8
gysagsohn <gysagsohn@hotmail.com>
2024-06-01 14:05:29 +1000

    styling updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
-
-
+import { DarkModeToggleButton } from "./DarkModeToggleButton";
+import '../styles/Footer.css'
 
 
 export default function Footer(){
 
 	return (
 		<footer>
-			<h6>Copyright 2024 ColourAcademy based on class work/ Gy Sohn</h6>
+			<h6>Copyright 2024 ColourAcademy / Alex</h6>
 			<a href="mailto:test@email.com">Email us!</a>
+
+			<DarkModeToggleButton />
 		</footer>
 	)
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import "../styles/Header.css";
+
 
 export default function Header(){
 
